6006f3c19fddd1fb17cbfada3e4ac82a347a9a43
Silas Sangmin <silas.sangmin@instantrad.com>
2025-07-20 20:39:44 +0000

    first commit

---
+++ backend/venture_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VentureTrackerConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "venture_tracker"
---
+++ backend/venture_tracker/urls.py
@@ -0,0 +1,7 @@
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    #path("auth/register", register_user, name="register"),
+]
